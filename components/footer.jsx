962bfac5a313d2529590a4b6e998a049db3a651e
hakimnazry <hakimnazry@gmail.com>
2024-05-30 17:06:50 +0800

    footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,9 +1,17 @@
 export default function Footer() {
-    return (
-        <>
-            <footer className="py-5 text-center font-semibold text-slate-500">
-                <p>Designed with <span className="text-red-600">♥</span> by <span className="text-blue-500">Motion-U</span></p>
-            </footer>
-        </>
-    )
+  return (
+    <>
+      <footer className="py-5 text-sm text-center font-semibold text-slate-500">
+        <p>
+          Designed with <span className="text-red-600">♥</span> by{" "}
+          <a href="https://www.motionu.club">
+            <span className="text-blue-500 underline hover:opacity-80">
+              Motion-U Club
+            </span>
+          </a>
+        </p>
+        <p>Br Hakim - hakimnazry@gmail.com</p>
+      </footer>
+    </>
+  );
 }
